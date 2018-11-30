219a4ce89ebbb00c05dd80138dced94148153b24
Cyclonecode <cyclonecode@gmail.com>
2018-11-30 02:24:22 +0100

    Add support to specify storeviews

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+import { currentStoreView, storeCodeFromRoute } from '@vue-storefront/core/store/lib/multistore'
+
 const EXTENSION_KEY = 'external-checkout'
 
 export default function (app, router, store, config) {
   const cmsUrl = config.externalCheckout.cmsUrl
+  const stores = config.externalCheckout.stores
+  const currentStore = currentStoreView()
 
   router.beforeEach((to, from, next) => {
-    if (to.name === 'checkout') {
-      window.location.replace(cmsUrl + '?token=' + store.state.user.token + '&cart=' + store.state.cart.cartServerToken)
+    let storeCode = storeCodeFromRoute(to)
+    if (config.storeViews.multistore === true) {
+      if (stores.indexOf(storeCode) > -1 && to.name === storeCode + '-checkout') {
+        console.log("redirecting to external checkout for " + storeCode)
+        window.location.replace(cmsUrl + '?token=' + store.state.user.token + '&cart=' + store.state.cart.cartServerToken)
+      } else {
+        next()
+      }
     } else {
-      next()   
-    } 
+      if (to.name === 'checkout') {
+        window.location.replace(cmsUrl + '?token=' + store.state.user.token + '&cart=' + store.state.cart.cartServerToken)
+      } else {
+        next()
+      }
+    }
   })
 
   return {
